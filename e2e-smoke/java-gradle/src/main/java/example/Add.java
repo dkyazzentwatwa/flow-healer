bac5591e9ea119fcbd6a7096a265a69f46cc1040
Cypher <93293468+dkyazzentwatwa@users.noreply.github.com>
2026-03-12 08:42:36 -0700

    healer: fix issue #946 (#949)

--- e2e-smoke/java-gradle/src/main/java/example/Add.java
+++ e2e-smoke/java-gradle/src/main/java/example/Add.java
@@ -6,4 +6,8 @@ public final class Add {
     public static int add(int left, int right) {
         return left + right;
     }
+
+    public static int add3(int left, int right, int extra) {
+        return left + right + extra;
+    }
 }
