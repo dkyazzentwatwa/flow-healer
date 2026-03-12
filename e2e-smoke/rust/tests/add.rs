2a5e3d99c2c8e751bba44fc55141446fd697e518
Cypher <93293468+dkyazzentwatwa@users.noreply.github.com>
2026-03-12 08:36:24 -0700

    healer: fix issue #945

--- e2e-smoke/rust/tests/add.rs
+++ e2e-smoke/rust/tests/add.rs
@@ -1,6 +1,11 @@
-use flow_healer_rust_smoke::add;
+use flow_healer_rust_smoke::{add, add_many};
 
 #[test]
 fn adds_two_numbers() {
     assert_eq!(add(2, 3), 5);
+}
+
+#[test]
+fn adds_many_numbers() {
+    assert_eq!(add_many(2, 3, 4), 9);
 }
