fe7b02e22fb6655a0f913bb7d9021db1ba0d8937
Cypher <93293468+dkyazzentwatwa@users.noreply.github.com>
2026-03-12 20:50:31 -0700

    healer: fix issue #987 (#998)

--- e2e-smoke/ruby/lib/add.rb
+++ e2e-smoke/ruby/lib/add.rb
@@ -1,3 +1,7 @@
 def add(left, right)
   left + right
 end
+
+def add_many(left, right, extra)
+  left + right + extra
+end
--- e2e-smoke/ruby/spec/add_spec.rb
+++ e2e-smoke/ruby/spec/add_spec.rb
@@ -5,3 +5,13 @@ RSpec.describe "#add" do
     expect(add(2, 3)).to eq(5)
   end
 end
+
+RSpec.describe "#add_many" do
+  it "adds positive integers" do
+    expect(add_many(2, 3, 4)).to eq(9)
+  end
+
+  it "adds mixed-sign integers" do
+    expect(add_many(-2, 3, -4)).to eq(-3)
+  end
+end
