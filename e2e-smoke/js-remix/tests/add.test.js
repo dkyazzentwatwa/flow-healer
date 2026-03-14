2ee15f46292266bcfae0fe7f947958d593fdd654
Cypher <93293468+dkyazzentwatwa@users.noreply.github.com>
2026-03-13 19:33:38 -0700

    healer: fix issue #1022 (#1057)

--- e2e-smoke/js-remix/tests/add.test.js
+++ e2e-smoke/js-remix/tests/add.test.js
@@ -4,6 +4,7 @@ const { add } = require('../app/utils/add.server');
 assert.strictEqual(add(1, 2), 3);
 assert.strictEqual(add('2', '5'), 7);
 assert.strictEqual(add(' 2 ', ' 5 '), 7);
+assert.strictEqual(add({ value: '4' }, 3), 7);
 assert.throws(() => add('nope', 1), {
   name: 'TypeError',
   message: 'add expects finite numeric inputs',
@@ -13,6 +14,10 @@ assert.throws(() => add('   ', 1), {
   message: 'add expects finite numeric inputs',
 });
 assert.throws(() => add(true, 1), {
+  name: 'TypeError',
+  message: 'add expects finite numeric inputs',
+});
+assert.throws(() => add({ value: 'nope' }, 1), {
   name: 'TypeError',
   message: 'add expects finite numeric inputs',
 });
